ed6387dcf847d84f442129d347244f1d6bc9c460
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2018-08-28 17:44:27 -0600

    updates to new plan

--- Planner/planner.go
+++ Planner/planner.go
@@ -7,80 +7,160 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"time"
 	//"reflect"
 
 	"github.com/goki/gi"
 	"github.com/goki/gi/gimain"
 	"github.com/goki/gi/giv"
-	"github.com/goki/gi/oswin"
+	"github.com/goki/gi/units"
+	"github.com/goki/ki/kit"
 	//"github.com/goki/gi/units"
 	"github.com/goki/ki"
 	//"github.com/goki/ki/kit"
 
-	bolt "github.com/coreos/bbolt"
 	"log"
 	//"time"
 )
 
+type PlanTime time.Time
+
+func (ft PlanTime) String() string {
+	return (time.Time)(ft).Format("Mon Jan  2 15 MST 2006")
+}
+
 type PlannerRec struct {
-	Goal string
-	Role string
+	Goal      string
+	Role      string
+	DoDate    PlanTime `desc:"date when you want to do it"`
+	Completed bool
 }
 
-var PlannerDB *bolt.DB
-
-func LoadPlannerTable() []*PlannerRec {
-
-	lt := make([]*PlannerRec, 0, 100) // 100 is the starting capacity of slice -- increase if you expect more users.
-
-	PlannerDB.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("PlannerTable"))
-
-		if b != nil {
-			b.ForEach(func(k, v []byte) error {
-				if v != nil {
-					rec := PlannerRec{}
-					json.Unmarshal(v, &rec) // loads v value as json into rec
-					lt = append(lt, &rec)   // adds record to login table
-
-				}
-				return nil
-			})
-		}
-		return nil
-	})
-
-	return lt
+func (pr *PlannerRec) Special(prompt string) {
+	fmt.Printf("this is a special function!  %v", prompt)
 }
 
-func CheckLogin(usr, passwd string) bool {
-	lt := LoadPlannerTable()
-	for _, lr := range lt {
-		if lr.Goal == usr && lr.Role == passwd {
-			return true
-		}
-	}
-	return false
-}
+type PlannerTable []*PlannerRec
 
-func SaveNewLogin(rec *PlannerRec) {
-	PlannerDB.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucketIfNotExists([]byte("PlannerTable"))
-		jb, err := json.Marshal(rec) // converts rec to json, as bytes jb
-
-		err = b.Put([]byte(rec.Goal), jb)
+func (pr *PlannerTable) SaveAs(filename gi.FileName) error {
+	b, err := json.MarshalIndent(pr, "", "  ")
+	if err != nil {
+		log.Println(err) // unlikely
 		return err
-	})
+	}
+	err = ioutil.WriteFile(string(filename), b, 0644)
+	if err != nil {
+		gi.PromptDialog(nil, gi.DlgOpts{Title: "Could not Save to File", Prompt: err.Error()}, true, false, nil, nil)
+		log.Println(err)
+	}
+	return nil
 }
+
+func (pr *PlannerTable) SaveDefault() error {
+	return pr.SaveAs("default_plan_table.json")
+}
+
+func (pr *PlannerTable) Load(filename gi.FileName) error {
+	b, err := ioutil.ReadFile(string(filename))
+	if err != nil {
+		gi.PromptDialog(nil, gi.DlgOpts{Title: "File Not Found", Prompt: err.Error()}, true, false, nil, nil)
+		log.Println(err)
+		return err
+	}
+	return json.Unmarshal(b, pr)
+}
+
+func (pr *PlannerTable) LoadDefault() error {
+	return pr.Load("default_plan_table.json")
+}
+
+var KiT_PlannerTable = kit.Types.AddType(&PlannerTable{}, PlannerTableProps)
+
+var PlannerTableProps = ki.Props{
+	"MainMenu": ki.PropSlice{
+		{"AppMenu", ki.BlankProp{}},
+		{"File", ki.PropSlice{
+			{"LoadDefault", ki.Props{
+				"shortcut": "Command+O",
+			}},
+			{"SaveDefault", ki.Props{
+				"shortcut": "Command+S",
+			}},
+			{"sep-close", ki.BlankProp{}},
+			{"Close Window", ki.BlankProp{}},
+		}},
+		{"Edit", "Copy Cut Paste"},
+		{"Window", "Windows"},
+	},
+	"ToolBar": ki.PropSlice{
+		{"SaveDefault", ki.Props{
+			"label": "Save",
+			"icon":  "file-save",
+		}},
+		{"LoadDefault", ki.Props{
+			"label": "Load",
+			"icon":  "file-save",
+		}},
+	},
+}
+
+var ThePlan PlannerTable
+
+// var PlannerDB *bolt.DB
+
+// func LoadPlannerTable() []*PlannerRec {
+
+// 	lt := make([]*PlannerRec, 0, 100) // 100 is the starting capacity of slice -- increase if you expect more users.
+
+// 	PlannerDB.View(func(tx *bolt.Tx) error {
+// 		b := tx.Bucket([]byte("PlannerTable"))
+
+// 		if b != nil {
+// 			b.ForEach(func(k, v []byte) error {
+// 				if v != nil {
+// 					rec := PlannerRec{}
+// 					json.Unmarshal(v, &rec) // loads v value as json into rec
+// 					lt = append(lt, &rec)   // adds record to login table
+
+// 				}
+// 				return nil
+// 			})
+// 		}
+// 		return nil
+// 	})
+
+// 	return lt
+// }
+
+// func CheckLogin(usr, passwd string) bool {
+// 	lt := LoadPlannerTable()
+// 	for _, lr := range lt {
+// 		if lr.Goal == usr && lr.Role == passwd {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
+
+// func SaveNewLogin(rec *PlannerRec) {
+// 	PlannerDB.Update(func(tx *bolt.Tx) error {
+// 		b, err := tx.CreateBucketIfNotExists([]byte("PlannerTable"))
+// 		jb, err := json.Marshal(rec) // converts rec to json, as bytes jb
+
+// 		err = b.Put([]byte(rec.Goal), jb)
+// 		return err
+// 	})
+// }
 
 func main() {
-	var err error
-	PlannerDB, err = bolt.Open("Planner.db", 0600, nil)
+	// var err error
+	// PlannerDB, err = bolt.Open("Planner.db", 0600, nil)
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer PlannerDB.Close()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer PlannerDB.Close()
 
 	gimain.Main(func() {
 		mainrun()
@@ -88,146 +168,140 @@ func main() {
 }
 
 func mainrun() {
-	width := 1024
-	height := 768
+	width := 1200
+	height := 900
 
-	// turn these on to see a traces of various stages of processing..
-	// gi.Update2DTrace = true
-	// gi.Render2DTrace = true
-	// gi.Layout2DTrace = true
-	// ki.SignalTrace = true
-
-	rec := ki.Node{}          // receiver for events
-	rec.InitName(&rec, "rec") // this is essential for root objects not owned by other Ki tree nodes
-
-	win := gi.NewWindow2D("gogi-widgets-demo", "Planner | Kataform", width, height, true) // true = pixel sizes
-
-	//icnm := "widget-wedge-down"
+	win := gi.NewWindow2D("kplanner", "Planner | Kataform", width, height, true) // true = pixel sizes
 
 	vp := win.WinViewport2D()
 	updt := vp.UpdateStart()
 	vp.Fill = true
 
-	// style sheet
-	var css = ki.Props{
-		"button": ki.Props{
-			"background-color": gi.Color{255, 240, 240, 255},
-		},
-		"#combo": ki.Props{
-			"background-color": gi.Color{240, 255, 240, 255},
-		},
-		".hslides": ki.Props{
-			"background-color": gi.Color{240, 225, 255, 255},
-		},
-		"kbd": ki.Props{
-			"color": "blue",
-		},
-	}
-	vp.CSS = css
+	// // style sheet
+	// var css = ki.Props{
+	// 	"button": ki.Props{
+	// 		"background-color": gi.Color{255, 240, 240, 255},
+	// 	},
+	// 	"#combo": ki.Props{
+	// 		"background-color": gi.Color{240, 255, 240, 255},
+	// 	},
+	// 	".hslides": ki.Props{
+	// 		"background-color": gi.Color{240, 225, 255, 255},
+	// 	},
+	// 	"kbd": ki.Props{
+	// 		"color": "blue",
+	// 	},
+	// }
+	// vp.CSS = css
 
-	vlay := vp.AddNewChild(gi.KiT_Frame, "vlay").(*gi.Frame)
-	vlay.Lay = gi.LayoutVert
-	vlay.SetProp("font-family", "Georgia, serif")
-	// vlay.SetProp("background-color", "linear-gradient(to top, red, lighter-80)")
-	// vlay.SetProp("background-color", "linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)")
-	// vlay.SetProp("background-color", "linear-gradient(to right, rgba(255,0,0,0), rgba(255,0,0,1))")
-	// vlay.SetProp("background-color", "radial-gradient(red, lighter-80)")
+	mfr := win.SetMainFrame()
+	mfr.SetProp("spacing", units.NewValue(1, units.Ex))
+	mfr.SetProp("font-family", "Georgia, serif")
 
-	trow := vlay.AddNewChild(gi.KiT_Layout, "trow").(*gi.Layout)
+	trow := mfr.AddNewChild(gi.KiT_Layout, "trow").(*gi.Layout)
 	trow.Lay = gi.LayoutVert
 	trow.SetStretchMaxWidth()
 
-	trow.AddNewChild(gi.KiT_Stretch, "str1")
 	title := trow.AddNewChild(gi.KiT_Label, "title").(*gi.Label)
-	title.Text =
-		`<b>Planner</b>`
+	title.Text = `<b>Planner</b>, based on the <b>7 habits of highly effective people</b>, using <b>weekly planning</b>, and <b>habit 3</b>"`
 	title.SetProp("text-align", gi.AlignCenter)
 	title.SetProp("align-vert", gi.AlignTop)
-	title.SetProp("font-family", "Times New Roman, serif")
 	title.SetProp("font-size", "x-large")
-	// title.SetProp("letter-spacing", 2)
-	title.SetProp("line-height", 1.5)
-	trow.AddNewChild(gi.KiT_Stretch, "str2")
 
-	p1 := trow.AddNewChild(gi.KiT_Label, "p1").(*gi.Label)
-	p1.Text = "<b>Planner</b>, based on the <b>7 habits of highly effective people</b>, using <b>weekly planning</b>, and <b>habit 3</b>"
-	p1.SetProp("text-align", gi.AlignCenter)
+	split := mfr.AddNewChild(gi.KiT_SplitView, "split").(*gi.SplitView)
 
-	///
-	motivationText := trow.AddNewChild(gi.KiT_Label, "motivationText").(*gi.Label)
-	motivationText.Text = "<b>Create Q2 goals for each of your roles! Make sure to do this weekly</b>"
-	motivationText.SetProp("text-align", gi.AlignCenter)
+	tv := split.AddNewChild(giv.KiT_TableView, "tv").(*giv.TableView)
+	tv.Viewport = vp
+	sv := split.AddNewChild(giv.KiT_StructView, "sv").(*giv.StructView)
+	sv.Viewport = vp
 
-	buttonStartResult := trow.AddNewChild(gi.KiT_Label, "buttonStartResult").(*gi.Label)
-	buttonStartResult.Text = "<b>Add new goal:</b>"
-	userText := trow.AddNewChild(gi.KiT_TextField, "userText").(*gi.TextField)
-	userText.SetText("Goal")
-	userText.SetProp("width", "20em")
-	passwdText := trow.AddNewChild(gi.KiT_TextField, "passwdText").(*gi.TextField)
-	passwdText.SetText("Role")
-	passwdText.SetProp("width", "20em")
+	split.SetSplits(.5, .5)
+	tv.SetSlice(&ThePlan, nil)
 
-	signUpButton := trow.AddNewChild(gi.KiT_Button, "signUpButton").(*gi.Button)
-	signUpButton.Text = "<b>Create!</b>"
-
-	signUpButton.ButtonSig.Connect(rec.This, func(recv, send ki.Ki, sig int64, data interface{}) {
-		//fmt.Printf("Received button signal: %v from button: %v\n", gi.ButtonSignals(sig), send.Name())
-		if sig == int64(gi.ButtonClicked) { // note: 3 diff ButtonSig sig's possible -- important to check
-			// vp.Win.Quit()
-			//gi.PromptDialog(vp, "Button1 Dialog", "This is a dialog!  Various specific types of dialogs are available.", true, true, nil, nil)
-			updt := vp.UpdateStart()
-			usr := userText.Text()
-			passwd := passwdText.Text()
-
-			newPlannerRec := PlannerRec{Goal: usr, Role: passwd}
-			SaveNewLogin(&newPlannerRec)
-
-			vp.UpdateEnd(updt)
+	tv.WidgetSig.Connect(sv.This, func(recv, send ki.Ki, sig int64, data interface{}) {
+		if sig == int64(gi.WidgetSelected) {
+			idx := tv.SelectedIdx
+			if idx >= 0 {
+				rec := ThePlan[idx]
+				sv.SetStruct(rec, nil)
+			}
 		}
 	})
+
+	// motivationText := trow.AddNewChild(gi.KiT_Label, "motivationText").(*gi.Label)
+	// motivationText.Text = "<b>Create Q2 goals for each of your roles! Make sure to do this weekly</b>"
+	// motivationText.SetProp("text-align", gi.AlignCenter)
+
+	// buttonStartResult := trow.AddNewChild(gi.KiT_Label, "buttonStartResult").(*gi.Label)
+	// buttonStartResult.Text = "<b>Add new goal:</b>"
+	// userText := trow.AddNewChild(gi.KiT_TextField, "userText").(*gi.TextField)
+	// userText.SetText("Goal")
+	// userText.SetProp("width", "20em")
+	// passwdText := trow.AddNewChild(gi.KiT_TextField, "passwdText").(*gi.TextField)
+	// passwdText.SetText("Role")
+	// passwdText.SetProp("width", "20em")
+
+	// signUpButton := trow.AddNewChild(gi.KiT_Button, "signUpButton").(*gi.Button)
+	// signUpButton.Text = "<b>Create!</b>"
+
+	// signUpButton.ButtonSig.Connect(rec.This, func(recv, send ki.Ki, sig int64, data interface{}) {
+	// 	//fmt.Printf("Received button signal: %v from button: %v\n", gi.ButtonSignals(sig), send.Name())
+	// 	if sig == int64(gi.ButtonClicked) { // note: 3 diff ButtonSig sig's possible -- important to check
+	// 		// vp.Win.Quit()
+	// 		//gi.PromptDialog(vp, "Button1 Dialog", "This is a dialog!  Various specific types of dialogs are available.", true, true, nil, nil)
+	// 		updt := vp.UpdateStart()
+	// 		usr := userText.Text()
+	// 		passwd := passwdText.Text()
+
+	// 		newPlannerRec := PlannerRec{Goal: usr, Role: passwd}
+	// 		SaveNewLogin(&newPlannerRec)
+
+	// 		vp.UpdateEnd(updt)
+	// 	}
+	// })
 
 	/*lt := LoadPlannerTable()
 
 	gi.StructTableView(lt)
 	*/
 	////
-	trow.AddNewChild(gi.KiT_Space, "spc1")
+	// trow.AddNewChild(gi.KiT_Space, "spc1")
 
-	viewlogins := trow.AddNewChild(gi.KiT_Button, "viewlogins").(*gi.Button)
-	viewlogins.SetText("View PlannerTable")
-	viewlogins.ButtonSig.Connect(rec.This, func(recv, send ki.Ki, sig int64, data interface{}) {
-		if sig == int64(gi.ButtonClicked) {
-			lt := LoadPlannerTable()
+	// viewlogins := trow.AddNewChild(gi.KiT_Button, "viewlogins").(*gi.Button)
+	// viewlogins.SetText("View PlannerTable")
+	// viewlogins.ButtonSig.Connect(rec.This, func(recv, send ki.Ki, sig int64, data interface{}) {
+	// 	if sig == int64(gi.ButtonClicked) {
+	// 		lt := LoadPlannerTable()
 
-			giv.TableViewDialog(vp, &lt, giv.DlgOpts{Title: "Login Table"}, nil, nil, nil)
-		}
-	})
+	// 		giv.TableViewDialog(vp, &lt, giv.DlgOpts{Title: "Login Table"}, nil, nil, nil)
+	// 	}
+	// })
 
-	addlogin := trow.AddNewChild(gi.KiT_Button, "addlogin").(*gi.Button)
-	addlogin.SetText("Add Login")
-	addlogin.ButtonSig.Connect(rec.This, func(recv, send ki.Ki, sig int64, data interface{}) {
-		if sig == int64(gi.ButtonClicked) {
-			rec := PlannerRec{}
-			giv.StructViewDialog(vp, &rec, giv.DlgOpts{Title: "Enter Login Info"}, recv, func(recv, send ki.Ki, sig int64, data interface{}) {
-				if sig == int64(gi.DialogAccepted) {
-					SaveNewLogin(&rec)
-				}
-			})
-		}
-	})
+	// addlogin := trow.AddNewChild(gi.KiT_Button, "addlogin").(*gi.Button)
+	// addlogin.SetText("Add Login")
+	// addlogin.ButtonSig.Connect(rec.This, func(recv, send ki.Ki, sig int64, data interface{}) {
+	// 	if sig == int64(gi.ButtonClicked) {
+	// 		rec := PlannerRec{}
+	// 		giv.StructViewDialog(vp, &rec, giv.DlgOpts{Title: "Enter Login Info"}, recv, func(recv, send ki.Ki, sig int64, data interface{}) {
+	// 			if sig == int64(gi.DialogAccepted) {
+	// 				SaveNewLogin(&rec)
+	// 			}
+	// 		})
+	// 	}
+	// })
 
-	quit := trow.AddNewChild(gi.KiT_Button, "quit").(*gi.Button)
-	quit.SetText("Quit")
-	quit.ButtonSig.Connect(rec.This, func(recv, send ki.Ki, sig int64, data interface{}) {
-		if sig == int64(gi.ButtonClicked) {
-			gi.PromptDialog(vp, gi.DlgOpts{Title: "Quit", Prompt: "Quit: Are You Sure?"}, true, true, recv, func(recv, send ki.Ki, sig int64, data interface{}) {
-				if sig == int64(gi.DialogAccepted) {
-					PlannerDB.Close()
-					oswin.TheApp.Quit()
-				}
-			})
-		}
-	})
+	// quit := trow.AddNewChild(gi.KiT_Button, "quit").(*gi.Button)
+	// quit.SetText("Quit")
+	// quit.ButtonSig.Connect(rec.This, func(recv, send ki.Ki, sig int64, data interface{}) {
+	// 	if sig == int64(gi.ButtonClicked) {
+	// 		gi.PromptDialog(vp, gi.DlgOpts{Title: "Quit", Prompt: "Quit: Are You Sure?"}, true, true, recv, func(recv, send ki.Ki, sig int64, data interface{}) {
+	// 			if sig == int64(gi.DialogAccepted) {
+	// 				PlannerDB.Close()
+	// 				oswin.TheApp.Quit()
+	// 			}
+	// 		})
+	// 	}
+	// })
 
 	vp.UpdateEndNoSig(updt)
 
